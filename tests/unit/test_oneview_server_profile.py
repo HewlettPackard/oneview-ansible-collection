574204c58a2996099cab6e30e2a48f57d755c42b
asisbagga <asisbagga@gmail.com>
2020-10-09 15:35:42 +0530

    sanity fix

--- tests/unit/test_oneview_server_profile.py
+++ tests/unit/test_oneview_server_profile.py
@@ -23,11 +23,11 @@ import pytest
 from copy import deepcopy
 from ansible_collections.hpe.oneview.tests.unit.utils.hpe_test_utils import OneViewBaseTest
 from ansible_collections.hpe.oneview.tests.unit.utils.oneview_module_loader import (ServerProfileModule,
-                                   OneViewModuleException,
-                                   OneViewModuleTaskError,
-                                   SPKeys,
-                                   ServerProfileMerger,
-                                   ServerProfileReplaceNamesByUris)
+                                                                                    OneViewModuleException,
+                                                                                    OneViewModuleTaskError,
+                                                                                    SPKeys,
+                                                                                    ServerProfileMerger,
+                                                                                    ServerProfileReplaceNamesByUris)
 
 
 SERVER_PROFILE_NAME = "Profile101"
