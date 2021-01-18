966d6b238243e6710150e44fed1b5c45eb9c9fc1
VenkateshRavula <venkatesh.ravula@hpe.com>
2021-01-18 17:01:43 +0530

    fix pylint errors

--- plugins/modules/oneview_server_hardware.py
+++ plugins/modules/oneview_server_hardware.py
@@ -52,7 +52,7 @@ options:
               C(multiple_servers_added) will add multiple rack-mount servers.
         choices: ['present', 'absent', 'power_state_set', 'refresh_state_set', 'ilo_firmware_version_updated',
                   'ilo_state_reset','uid_state_on', 'uid_state_off',  'enable_maintenance_mode', 'disable_maintenance_mode',
-                  'environmental_configuration_set','environmental_configuration_set', 'multiple_servers_added']
+                  'environmental_configuration_set', 'multiple_servers_added']
         required: true
         type: str
     data:
