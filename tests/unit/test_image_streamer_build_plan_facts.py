9aca0ae1e6e0632dc082a7ab6c77d932696da640
Shanmugam <m-.shanmugam@hpe.com>
2021-03-09 15:05:11 +0530

    fixed_pylint

--- tests/unit/test_image_streamer_build_plan_facts.py
+++ tests/unit/test_image_streamer_build_plan_facts.py
@@ -16,6 +16,9 @@
 # limitations under the License.
 ###
 
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
 import pytest
 
 from ansible_collections.hpe.oneview.tests.unit.utlis.hpe_test_utils import ImageStreamerBaseFactsTest
