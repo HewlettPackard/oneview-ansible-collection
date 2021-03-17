e9240e1ab3e1032feb6a94ddbcb6b433ce0bd346
Shanmugam <m-.shanmugam@hpe.com>
2021-03-17 18:32:17 +0530

    modified testcases

--- tests/unit/test_oneview_appliance_device_snmp_v1_trap_destinations.py
+++ tests/unit/test_oneview_appliance_device_snmp_v1_trap_destinations.py
@@ -20,8 +20,8 @@ __metaclass__ = type
 
 import pytest
 
-from ansible_collections.hpe.oneview.tests.unit.utils.hpe_test_utils import OneViewBaseTest
-from ansible_collections.hpe.oneview.tests.unit.utils.oneview_module_loader import ApplianceDeviceSnmpV1TrapDestinationsModule
+from utils.hpe_test_utils import OneViewBaseTest
+from utils.oneview_module_loader import ApplianceDeviceSnmpV1TrapDestinationsModule
 
 ERROR_MSG = 'Fake message error'
 
--- tests/unit/test_oneview_appliance_device_snmp_v1_trap_destinations_facts.py
+++ tests/unit/test_oneview_appliance_device_snmp_v1_trap_destinations_facts.py
@@ -20,8 +20,8 @@ __metaclass__ = type
 
 import pytest
 
-from ansible_collections.hpe.oneview.tests.unit.utils.hpe_test_utils import OneViewBaseFactsTest
-from ansible_collections.hpe.oneview.tests.unit.utils.oneview_module_loader import ApplianceDeviceSnmpV1TrapDestinationsFactsModule
+from utils.hpe_test_utils import OneViewBaseFactsTest
+from utils.oneview_module_loader import ApplianceDeviceSnmpV1TrapDestinationsFactsModule
 
 ERROR_MSG = 'Fake message error'
 
