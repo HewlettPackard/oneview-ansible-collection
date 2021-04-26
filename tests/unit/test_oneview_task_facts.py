1de8e268198e45a75f106f12385e07dae0fa806b
yuvirani <yuvaranisrec@gmail.com>
2021-04-26 19:45:19 +0530

    fix path

--- tests/unit/test_oneview_task_facts.py
+++ tests/unit/test_oneview_task_facts.py
@@ -22,7 +22,7 @@ __metaclass__ = type
 import pytest
 
 from ansible_collections.hpe.oneview.tests.unit.utils.hpe_test_utils import OneViewBaseFactsTest
-from ansible_collections.hpe.oneview.tests.unit.utils.hpe_test_utils.oneview_module_loader import TaskFactsModule
+from ansible_collections.hpe.oneview.tests.unit.utils.oneview_module_loader import TaskFactsModule
 
 ERROR_MSG = 'Fake message error'
 
