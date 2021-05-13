676a6ca33b5ebc5529b1b9517acea77d3f14333b
VenkateshRavula <venkatesh.ravula@hpe.com>
2021-05-13 16:55:45 +0530

    fix pep8 errors

--- tests/unit/test_oneview_server_hardware.py
+++ tests/unit/test_oneview_server_hardware.py
@@ -221,7 +221,7 @@ class TestServerHardwareModule(OneViewBaseTest):
 
     def test_should_add_multiple_servers(self):
         self.resource.get_by_name.return_value = None
-        self.resource.get_all.return_value = [{'name':'172.18.6.16'}]
+        self.resource.get_all.return_value = [{'name': '172.18.6.16'}]
         self.resource.data = {'name': 'name'}
 
         self.resource.add_multiple_servers.return_value = self.resource
@@ -238,7 +238,7 @@ class TestServerHardwareModule(OneViewBaseTest):
 
     def test_should_not_add_multiple_servers_when_already_present(self):
         self.resource.get_by_name.return_value = None
-        self.resource.get_all.return_value = [{'name':'172.18.6.15'}]
+        self.resource.get_all.return_value = [{'name': '172.18.6.15'}]
         self.resource.data = {'name': 'name'}
 
         self.resource.add_multiple_servers.return_value = self.resource
