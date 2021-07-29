7039d06e1e9649f1b2e829e42222a41421cf90f0
yuvirani <yuvaranisrec@gmail.com>
2021-07-29 14:37:40 +0530

    Fix UT

--- tests/unit/test_oneview_appliance_network_interfaces.py
+++ tests/unit/test_oneview_appliance_network_interfaces.py
@@ -43,6 +43,7 @@ DEFAULT_PARAMS = {
     ]
 }
 UPDATE_DNS = {
+    "macAddress": "00:00:11:28:j8:90",
     "ipv4NameServers": [
         "16.17.18.21",
         "16.17.18.22"
@@ -93,7 +94,7 @@ class TestApplianceNetworkInterfacesModule(OneViewBaseTest):
         )
 
     def test_should_update_when_network_interface_has_different_attributes(self):
-   
+
         network_data = DEFAULT_PARAMS.copy()
         network_data['ipv4NameServers'] = ['16.17.18.21', '16.17.18.22']
         self.resource.get_by_mac_address.return_value = self.resource
