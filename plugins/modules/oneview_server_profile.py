49a7473393e9eab13576dc46f99db4d7f908e43d
Chebrolu Bala Sai Harika <34155580+chebroluharika@users.noreply.github.com>
2021-09-30 15:52:55 +0530

    Update oneview_server_profile.py

--- plugins/modules/oneview_server_profile.py
+++ plugins/modules/oneview_server_profile.py
@@ -534,8 +534,7 @@ class ServerProfileModule(OneViewModule):
         else:
             user_name = self.oneview_client._OneViewClient__connection._cred['userName']
             scope_user = self.oneview_client.users.get_by_userName(user_name)
-            user_name_string = 'Administrator'
-            if scope_user and user_name != user_name_string.lower():
+            if (scope_user and user_name != 'Administrator' and user_name != 'administrator'):
                 permissions = scope_user.data["permissions"]
                 scope_uris = set([each_role.get("scopeUri") for each_role in permissions])
                 scope_uri = '%20OR%20'.join(list(scope_uris))
