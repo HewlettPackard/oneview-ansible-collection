3e533cfbf645717841916f037e4abd1be9fecb47
akshith-gunasheelan <akshith.gunasheelan@hpe.com>
2026-04-29 09:19:24 +0530

    Licsence changes

--- plugins/modules/oneview_appliance_device_snmp_v3_users.py
+++ plugins/modules/oneview_appliance_device_snmp_v3_users.py
@@ -3,6 +3,8 @@
 # Copyright: (c) 2021-2024, Hewlett Packard Enterprise Development LP
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
+from __future__ import (absolute_import, division, print_function)
+
 __metaclass__ = type
 ANSIBLE_METADATA = {'status': ['stableinterface'],
                     'supported_by': 'community',
